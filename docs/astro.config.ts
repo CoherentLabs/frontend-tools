397801b567af1c947eb0cfde9675e05327a6614f
Kaloyan Geshev <kaloyan.g@coherent-labs.com>
2025-05-15 11:24:22 +0300

    Added docs for the tools (#8)
    * Added docs for the tools
    
    * Added additional docs for the framework
    
    * Updated versions of the packages and fixed publish action

---
+++ docs/astro.config.ts
@@ -0,0 +1,161 @@
+import starlight from '@astrojs/starlight'
+import { defineConfig } from 'astro/config'
+import starlightThemeRapide from 'starlight-theme-rapide'
+import starlightLinksValidator from 'starlight-links-validator';
+import starlightHeadingBadges from 'starlight-heading-badges'
+import starlightSidebarTopics from 'starlight-sidebar-topics'
+import generateChangelog from './src/changelogSideBar';
+import fs from 'fs';
+
+function generateVersion(version, link) {
+  const config: { label: string; link?: string; attrs?: { target: string }; badge?: { text: string; variant: string } } = {
+    label: 'Version:',
+    badge: {
+      text: `${version}`,
+      variant: 'tip',
+    },
+  }
+
+  if (link) {
+    config.link = link;
+    config.attrs = { target: '_blank' };
+  }
+
+  return config;
+}
+
+async function generateVersionWithPackageJSON(packagePath, link) {
+  if (!fs.existsSync(packagePath)) throw new Error(`Version not found in ${packagePath}`);
+
+  const packageContent = fs.readFileSync(packagePath, 'utf-8');
+  const packageJson = JSON.parse(packageContent);
+  const version = packageJson?.version;
+  if (!version) throw new Error(`Version not defined in ${packagePath}`);
+
+  return generateVersion(version, link);
+}
+
+async function getConfig() {
+  const documentations = ['e2e', 'gameface-vite-plugin', 'vite-solid-style-to-css-plugin'];
+
+  const sideBarTopics = [
+    {
+      link: '/e2e/getting-started',
+      label: 'Gameface E2E',
+      id: 'e2e',
+      icon: 'rocket',
+      items: [
+        await generateVersionWithPackageJSON(
+          '../e2e/package.json',
+          'https://npmjs.org/gameface-e2e'
+        ),
+        {
+          label: 'Gettings Started',
+          autogenerate: { directory: 'e2e/getting-started' },
+        },
+        {
+          label: 'Concepts',
+          autogenerate: { directory: 'e2e/concepts' },
+        },
+        generateChangelog('e2e'),
+        {
+          label: 'API Reference',
+          collapsed: true,
+          autogenerate: { directory: 'e2e/api', collapsed: false },
+
+        },
+      ],
+    },
+    {
+      link: '/gameface-vite-plugin/getting-started',
+      label: 'Gameface Vite Plugin',
+      id: 'gameface-vite-plugin',
+      icon: 'seti:vite',
+      items: [
+        await generateVersionWithPackageJSON(
+          '../gameface-ui-vite-plugins/vite-gameface/package.json',
+          'https://npmjs.org/vite-gameface'
+        ),
+        {
+          label: 'Gettings Started',
+          autogenerate: { directory: 'gameface-vite-plugin/getting-started' },
+        },
+        {
+          label: 'Concepts',
+          autogenerate: { directory: 'gameface-vite-plugin/concepts' },
+        },
+        generateChangelog('gameface-vite-plugin'),
+      ],
+    },
+    {
+      link: '/vite-solid-style-to-css-plugin/getting-started',
+      label: 'Solid Style to CSS Plugin',
+      id: 'vite-solid-style-to-css-plugin',
+      icon: 'seti:css',
+      items: [
+        await generateVersionWithPackageJSON(
+          '../gameface-ui-vite-plugins/vite-solid-style-to-css/package.json',
+          'https://npmjs.org/vite-solid-style-to-css'
+        ),
+        {
+          label: 'Gettings Started',
+          autogenerate: { directory: 'vite-solid-style-to-css-plugin/getting-started' },
+        },
+        {
+          label: 'Concepts',
+          autogenerate: { directory: 'vite-solid-style-to-css-plugin/concepts' },
+        },
+        generateChangelog('vite-solid-style-to-css-plugin'),
+      ],
+    },
+  ];
+
+  return defineConfig({
+    redirects: documentations.reduce((acc, doc) => {
+      acc[`/${doc}`] = `/${doc}/getting-started/`;
+      return acc;
+    }, {}),
+    integrations: [
+      starlight({
+        favicon: '/favicon-32x32.png',
+        logo: {
+          dark: './src/assets/gameface-ui-header-dark.svg',
+          light: './src/assets/gameface-ui-header-light.svg',
+          replacesTitle: true
+        },
+        components: {
+          Sidebar: './src/components/Sidebar.astro',
+        },
+        credits: false,
+        customCss: ['./src/styles/custom.css'],
+        plugins: [
+          starlightThemeRapide(),
+          starlightLinksValidator(),
+          starlightHeadingBadges(),
+          starlightSidebarTopics(sideBarTopics, {
+            topics: documentations.reduce((acc, doc) => {
+              acc[doc] = [`/${doc}/changelog`];
+              return acc;
+            }, {})
+          })
+        ],
+        social: [
+          {
+            icon: 'open-book',
+            label: 'Documentation',
+            href: 'https://coherent-labs.com/documentation',
+          },
+          {
+            icon: 'email',
+            label: 'Email',
+            href: 'https://coherent-labs.com/get-in-touch'
+          },
+        ],
+        title: 'Gameface Frontend Tools',
+      }),
+    ],
+    site: 'https://frontend-tools.coherent-labs.com',
+  })
+}
+
+export default getConfig();
--- gameface-ui-vite-plugins/vite-gameface/README.md
+++ gameface-ui-vite-plugins/vite-gameface/README.md
@@ -1,3 +1,5 @@
 # Vite Gameface Plugin
 
 A Vite plugin designed to streamline the development of Gameface UI projects by providing optimized build and development configurations.
+
+You can read the full documentation for the plugin [here](https://frontend-tools.coherent-labs.com/gameface-vite-plugin/getting-started/).
