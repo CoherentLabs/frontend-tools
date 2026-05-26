0fcbc1c7ee90a0fe655aeb0a9639753042d812e3
alien1976 <kaloyan.g@coherent-labs.com>
2026-05-26 14:29:19 +0300

    Documented eslint plugin

--- eslint-gameface/examples/sample.jsx
+++ eslint-gameface/examples/sample.jsx
@@ -1,7 +1,7 @@
 export function Sample() {
   return (
     <div style={{ 'align-content': "space", test: 'alabala', accentColor: 'test' }}>
-      <p style="align: space-between;">Example JSX with embedded CSS (supported properties only).</p>
+      <p style="align-content: space-between;">Example JSX with embedded CSS (supported properties only).</p>
       <p style={{ fontSize: "min(12px, 16px)" }}>unsupported min() in inline style</p>
     </div>
   );
